f6ec9451e424ce54931d8238a8f964ca9682e9f3
Youri Tolstoy <ytolstoy@gmail.com>
2016-02-04 12:32:30 +0100

    fix(lpm): several little bugs here and there

--- test/dummy-supervisor.js
+++ test/dummy-supervisor.js
@@ -1,7 +1,38 @@
-var childProcess = require('child_process');
+process.on('uncaughtException', function (e) {
+    console.log(e.stack);
+});
 
-var p = childProcess.fork(__dirname + '/dummy-worker.js');
+process.on('unhandledException', function (e) {
+    console.log(e.stack);
+});
 
-p.on('message', function (data) {
-    console.log(data);
-})
+var supervisor = require('../index.js').supervisor({
+    worker: {
+        file: __dirname + '/dummy-worker.js',
+        count: 2
+    }
+});
+
+supervisor.on('SIGINT', function () {
+    console.log('SIGINT received through supervisor');
+    supervisor.softKill(function () {
+        console.log('soft kill done')
+    }).then(function () {
+        console.log('soft kill done promised')
+    });
+});
+
+supervisor.on('online', function () {
+    console.log('All workers are online');
+    setTimeout(function () {
+        for (var i = 0; i < 10; i++) {
+            supervisor.enqueue({salut: 'salue'}).then((function (i) {
+                return function (r) {
+                    console.log(i, r);
+                }
+            })(i));
+        }
+    }, 2000);
+});
+
+supervisor.start();
--- test/dummy-worker.js
+++ test/dummy-worker.js
@@ -1,3 +1,36 @@
+process.on('uncaughtException', function (e) {
+    console.log(e.stack);
+});
+
+process.on('unhandledException', function (e) {
+    console.log(e.stack);
+});
+
 var worker = require('../index.js').worker;
+
+var count = 0;
+
+worker.on('request', function (req) {
+    count++;
+    console.log(req.data);
+    setTimeout((function (count) {
+        return function () {
+            req.respond({coucou: 'toi', index: count});
+        }
+    })(count), 1000);
+});
+var t = '';
+if (Math.random() > 0.6) {
+    console.log('eating memory');
+    for (i = 0; i < 1000000; i++) {
+        t += 'bqeuifgqlizeuuiqlzfbeilquzqjfbqeuifgqlizeuuiqlzfbeilquzqjfbqeuifgqlizeuuiqlzfbeilquzqjfbqeuifgqlizeuuiqlzfbeilquzqjfbqeuifgqlizeuuiqlzfbeilquzqjfbqeuifgqlizeuuiqlzfbeilquzqjfbqeuifgqlizeuuiqlzfbeilquz';
+    }
+}
+
+console.log(process.memoryUsage());
+
+//worker.on('softKill', function (done) {
+//    setTimeout(done, 2000);
+//});
 
 worker.ready();
